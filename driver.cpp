bdaa16ba38e5ef17e495a663d2785767d5a24733
Kevin-Ngo <kngo1207@verizon.net>
2017-12-01 19:05:46 -0800

    some driver stuff, like entry

--- driver.cpp
+++ driver.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "AVLTree.h"
 using namespace std;
+
+void fillTree(AVLTree& t, string fileName);
+void stringToEntry(string s, Entry& e);
 
 int main()
 {
 	typedef Entry E;
 	AVLTree newTree;
-	E uno(20, 1, "asdf");
+	fillTree(newTree, "p4small.txt");
+	/*E uno(20, 1, "asdf");
 	E dos(55, 21, "asdf");
 	E tres(13, 123, "123");
 	E quatro(10, 1234, "1234");
@@ -21,12 +26,68 @@ int main()
 	newTree.insert(seis);
 	SearchTree::Iterator it(newTree.begin());
 	E output;
+	*/
+	SearchTree::Iterator it(newTree.begin());
+	E output;
 	for (it; it != newTree.end(); ++it)
 	{
 		output = *it;
 		output.printData();
 	}
+	string s;
+	cin >> s;
 	return 0;
+}
 
+void fillTree(AVLTree& t, string fileName)
+{
+	Entry E;
+	string record;
+	fstream infile;
+	infile.open(fileName);
+	if (infile.is_open())
+	{
+		while (!infile.eof())
+		{
+			getline(infile, record);
+			stringToEntry(record, E);
+			t.insert(E);
+		}
+	}
+	else
+		cout << "Incorrect file name passed or does not exist.\n";
+}
 
+void stringToEntry(string s, Entry& e)
+{
+	int sSize = s.length(), lowerBound = 0, upperBound, dummyV;
+	bool commaOne = false, commaTwo = false;
+	string subString;
+	for (int i = 0; i < sSize; i++)
+	{
+		if ((s[i] == ',') && (commaOne != true))
+		{
+			commaOne = true;
+			upperBound = i;
+			subString = s.substr(lowerBound, upperBound - lowerBound);
+			lowerBound = ++upperBound;
+			dummyV = atoi(subString.c_str());
+			e.county_state_code = dummyV;
+		}
+		else if ((s[i] == ',') && (commaTwo != true) && (commaOne == true))
+		{
+			commaTwo = true;
+			upperBound = i;
+			subString = s.substr(lowerBound, upperBound - lowerBound);
+			lowerBound = upperBound + 2;
+			dummyV = atoi(subString.c_str());
+			e.population = dummyV;
+		}
+		if ((s[i] == '\"') && (commaOne && commaTwo))
+		{
+			upperBound = i;
+			subString = s.substr(lowerBound, upperBound - lowerBound);
+			e.county_state_name = subString;
+		}
+	}
 }
